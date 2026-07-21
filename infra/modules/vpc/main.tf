c80b4060b4be52d1fb7aca0659ced2d9b08179f6
Sakariya_Aden <zlac406@live.rhul.ac.uk>
2026-07-21 19:20:33 +0100

    feat: add elastic ip and nat gateway for private subnet internet access

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -69,3 +69,25 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+
+# Elastic IP
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip"
+  }
+}
+
+# Nat Gateway
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project_name}-nat-gateway"
+  }
+}
